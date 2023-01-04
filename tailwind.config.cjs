a1b5b50ba049b4e088c4696120db414547faeec3
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-01-04 13:56:37 -0300

    update setup project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      poppins: ["Poppins", "sans-serif"]
+    },
   },
   plugins: [],
 }

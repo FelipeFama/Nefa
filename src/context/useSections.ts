7d95333cc7c7e305311b8affdb27e7748d51393c
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-01-06 21:34:31 -0300

    Update useSections.ts

--- src/context/useSections.ts
+++ src/context/useSections.ts
@@ -1,5 +1,13 @@
 import logoImg from "../assets/images/logo.svg";
 import globeImg from "../assets/images/illustrations/globe.png";
+import buyImg from "../assets/images/illustrations/buy.png";
+import creditCardImg from "../assets/images/illustrations/credit-card.png";
+import tradingToolImg from "../assets/images/illustrations/tradingtools.png";
+import banksImg from "../assets/images/illustrations/banks.png";
+import signUpImg from "../assets/images/illustrations/signup.png";
+import walletImg from "../assets/images/illustrations/wallet.png";
+import okayImg from "../assets/images/illustrations/okay.png";
+import faqImg from "../assets/images/illustrations/faq.png";
 
 export const logo = {
   logoImage: {
@@ -16,5 +24,47 @@ export const illustrations = {
       source: globeImg,
       alt: "crypto globe",
     },
+  },
+  buyImage: {
+    image: {
+      source: buyImg,
+      alt: "buy crypto",
+    },
+  },
+  creditCardImage: {
+    image: {
+      source: creditCardImg,
+      alt: "credit card",
+    },
+  },
+  tradingToolImage: {
+    image: {
+      source: tradingToolImg,
+      alt: "trading tool",
+    },
+  },
+  bankImage: {
+    image: {
+      source: banksImg,
+      alt: "cryptocurrency vault",
+    },
+  },
+  signUpImage: {
+    image: {
+      source: signUpImg,
+      alt: "mouse clicking on cryptocurrency",
+    },
+  },
+  walletImage: {
+    image: walletImg,
+    alt: "cryptocurrency wallet",
+  },
+  okayImage: {
+    image: okayImg,
+    alt: "hand holding cryptocurrency",
+  },
+  faqImage: {
+    image: faqImg,
+    alt: "faq criptocurrency",
   },
 };

45b3992cb7500ce6d9e6e60601888ac021f05593
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-05-27 23:27:53 -0300

    clean project

--- nefa-frontend/tailwind.config.js
+++ nefa-frontend/tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    colors: {
+      primary: "#468EF9",
+      secondary: "#0C66EE",
+      blue: "#2F7CF0",
+      black: "#222222",
+      white: "#ffff",
+      gray: "#666666",
+      lightgray: "#DDDDDD",
+      green: "#28C165",
+      red: "#F4574D",
     },
   },
   plugins: [],
-}
+};

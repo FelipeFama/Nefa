87f062667a4983ba739d8ea275a10f4fbc118128
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-06-09 20:55:34 -0300

    Update header

--- nefa-frontend/src/components/layout/Header/Menu.tsx
+++ nefa-frontend/src/components/layout/Header/Menu.tsx
@@ -8,67 +8,37 @@ interface MenuProps {
 
 export function Menu({ header }: MenuProps) {
   const item = header[0];
-  const menuList = item.links;
+  const menuList: any = item.links;
+
+  const dropdownMenuItems = [
+    { label: "Exchanges", href: "#" },
+    { label: "Wallets", href: "#" },
+  ];
+
   return (
     <>
       <ul className="flex md:gap-8 gap-4 max-lg:flex-col">
-        <li>
-          <Link
-            className="hover:text-primary transition ease-in-out delay-100"
-            href="#"
-          >
-            {menuList[0]}
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="hover:text-primary transition ease-in-out delay-100"
-            href="#"
-          >
-            {menuList[1]}
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="hover:text-primary transition ease-in-out delay-100"
-            href="#"
-          >
-            {menuList[2]}
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="hover:text-primary transition ease-in-out delay-100"
-            href="#"
-          >
-            {menuList[3]}
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="hover:text-primary transition ease-in-out delay-100"
-            href="#"
-          >
-            {menuList[4]}
-          </Link>
-        </li>
+        {menuList.map((menuItem: string, index: number) => (
+          <li key={index}>
+            <Link
+              className="hover:text-primary transition ease-in-out delay-100"
+              href="#"
+            >
+              {menuItem}
+            </Link>
+          </li>
+        ))}
         <DropdownMenu className="" onClick={undefined}>
-          <div className="py-4 max-md:-left-24">
-            <Link
-              className="hover:text-primary transition ease-in-out delay-100"
-              href="#"
-            >
-              Exchanges
-            </Link>
-          </div>
-          <div className="py-4 max-md:-left-24">
-            <Link
-              className="hover:text-primary transition ease-in-out delay-100"
-              href="#"
-            >
-              Wallets
-            </Link>
-          </div>
+          {dropdownMenuItems.map((item, index) => (
+            <div key={index} className="py-4 max-md:-left-24">
+              <Link
+                className="hover:text-primary transition ease-in-out delay-100"
+                href={item.href}
+              >
+                {item.label}
+              </Link>
+            </div>
+          ))}
         </DropdownMenu>
       </ul>
     </>

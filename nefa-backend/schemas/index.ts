ed4196b404ec52ffb75a77a7b6dbb28e0582f3ee
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-08-31 22:53:02 -0300

    feat: add new schemas step and faq

--- nefa-backend/schemas/index.ts
+++ nefa-backend/schemas/index.ts
@@ -6,5 +6,18 @@ import partners from './partners'
 import credit from './credit'
 import trading from './trading'
 import security from './security'
+import step from './step'
+import faq from './faq'
 
-export const schemaTypes = [header, hero, coins, buy, partners, credit, trading, security]
+export const schemaTypes = [
+  header,
+  hero,
+  coins,
+  buy,
+  partners,
+  credit,
+  trading,
+  security,
+  step,
+  faq,
+]
---
+++ nefa-backend/schemas/step.ts
@@ -0,0 +1,55 @@
+export default {
+  name: 'step',
+  title: 'Step',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'object',
+      fields: [
+        {
+          name: 'text1',
+          title: 'Text1',
+          type: 'string',
+        },
+      ],
+    },
+    {
+      name: 'article',
+      title: 'Article',
+      type: 'array',
+      of: [
+        {
+          name: 'article',
+          title: 'Article',
+          type: 'object',
+          fields: [
+            {
+              name: 'image',
+              title: 'Image',
+              type: 'image',
+              fields: [
+                {
+                  name: 'alt',
+                  title: 'Alt',
+                  type: 'string',
+                },
+              ],
+            },
+            {
+              name: 'title',
+              title: 'Title',
+              type: 'string',
+            },
+            {
+              name: 'content',
+              title: 'Content',
+              type: 'text',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}

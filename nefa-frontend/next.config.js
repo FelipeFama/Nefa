fe4da11ea5cff232773507cda23ef6d23bcef1cd
FelipeFama <lipehfama@gmail.com>
2024-01-12 00:01:05 -0300

    update project

--- nefa-frontend/next.config.js
+++ nefa-frontend/next.config.js
@@ -10,10 +10,12 @@ const nextConfig = {
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  disable: process.env.NODE_ENV === "production",
+  disable: process.env.NODE_ENV === "development",
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
+  register: true,
+  scope: "/src",
   sw: "service-worker.js",
 });
 
